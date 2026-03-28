6bf421f8fb2842504c3869b1ba14c6cd752e0208
Brian Jung <jungsbro@gmail.com>
2026-03-28 11:33:04 +0900

    [fix] debian13에서 fzf_select 애러부분 수정

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -77,22 +77,31 @@ class fzf_select(Command):
     """
     def execute(self):
         import subprocess
+        import os
+
+        exclude_part = r"\( -path '*/.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune"
+
         if self.quantifier:
             # match only directories
-            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            # command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            # -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            command = f"find -L . {exclude_part} -o -type d -print 2> /dev/null | fzf +m"
         else:
             # match files and directories
-            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            # command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            # -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+            command = f"find -L . {exclude_part} -o -print 2> /dev/null | fzf +m"
+
         fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
+
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
-            if os.path.isdir(fzf_file):
-                self.fm.cd(fzf_file)
-            else:
-                self.fm.select_file(fzf_file)
+            if os.path.exists(fzf_file):
+                if os.path.isdir(fzf_file):
+                    self.fm.cd(fzf_file)
+                else:
+                    self.fm.select_file(fzf_file)
 # ------------------------------------------------------------------------------
 
 class fzf_locate(Command):
